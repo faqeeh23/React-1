ad65b24ec9d40fbe2ca9d747434ee83b3a2657d5
Mohammed Al-faqeeh <mohammadaf200477@gmail.com>
2025-09-28 14:08:56 +0300

    .

--- react-1/src/cpmponants/products.js
+++ react-1/src/cpmponants/products.js
@@ -6,6 +6,7 @@ import headphone from "../image/headphone.jpg"
 import smartwach from "../image/smartwach.jpg"
 import AppleSmartWatch from "../image/AppleSmartWatch.jpg"
 import gamingHeadphone from "../image/gamingHeadphone.jpg"
+import { CiSearch } from "react-icons/ci";
 
 export default function Products() {
     
@@ -21,23 +22,29 @@ export default function Products() {
         {id :4 , name :"Apple smart wach" ,img:AppleSmartWatch, price:400 , details:"apple smartwach"},
         {id :5 , name :"Gaming Headphone" ,img:gamingHeadphone, price:500 , details:"gaming headphone"},
     ]
+
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const handleChange = (event) => {
+        const searchTetm = event.target.value;
+        setSearchTerm(searchTetm);
+
+        const filtered = subProducts.filter((item) =>
+            item.name.toLowerCase().includes(searchTetm.toLowerCase())
+        );
+        setFilteredProducts(filtered);
+    }
+
+    const [filteredProducts, setFilteredProducts] = React.useState(subProducts);
+
+
     return ( 
         <div className="products">
-            <div className="main-products">
-                <h1>Our Products </h1>
-                <button onClick={handelcklick} className="btn_show">{showImage === 'yes' ? "Hide Products" : "Show Products"}</button>
-                {showImage == 'yes' &&  (
-                    <div className="product-images"> 
-                        <img src={smartwach}></img>
-                        <img src={headphone}></img>
-                    </div>
-                )
-                }
+            <div className="seach">
+                <input type="text" placeholder="Search..." value={searchTerm} onChange={handleChange}/>
             </div>
-            <h1>Main Product</h1>
             <div id="subProducts">
                 <ul>
-                    {subProducts.map((item) => (
+                    {filteredProducts.map((item) => (
                         <li key={item.id}>
                             <img src={item.img}  alt={item.name} />
                             <h4>{item.name}</h4>
@@ -47,6 +54,18 @@ export default function Products() {
                         </li>
                     ))}
                 </ul>
+            </div>
+            <hr></hr>
+            <div className="main-products">
+                <h1>Top Products </h1>
+                <button onClick={handelcklick} className="btn_show">{showImage === 'yes' ? "Hide Products" : "Show Products"}</button>
+                {showImage == 'yes' &&  (
+                    <div className="product-images"> 
+                        <img src={smartwach}></img>
+                        <img src={headphone}></img>
+                    </div>
+                )
+                }
             </div>
         </div>    
     )
